519146340ef6e1347bcc333f1cdb89287e26ae83
Amaral <guilhermeamaral81@hotmail.com>
2021-08-29 03:29:51 -0300

    adiciona script start

--- Back-End/src/index.js
+++ Back-End/src/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(routes);
-app.listen('5000', () => {
-    console.log('rodando na porta 5000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.en);
 });
 
